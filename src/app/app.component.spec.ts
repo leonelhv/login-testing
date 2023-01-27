737078510aefaeda1b85f1d4e12c5bde3078c246
Leonel Huachaca <leonel.huachacav@gmail.com>
2023-01-27 10:24:52 -0500

    add tests form

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
+      imports: [ReactiveFormsModule],
     }).compileComponents();
   });
 
@@ -16,16 +15,26 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'login-testing'`, () => {
+  it('should return invalid formLogin', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('login-testing');
-  });
+    const mockData = {
+      usuario: '',
+      password: '',
+    };
 
-  it('should render title', () => {
+    app.formLogin.setValue(mockData);
+    expect(app.formLogin.invalid).toBe(true);
+  });
+  it('should return valid formLogin', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('login-testing app is running!');
+    const app = fixture.componentInstance;
+    const mockData = {
+      usuario: 'usuario1',
+      password: '1234',
+    };
+
+    app.formLogin.setValue(mockData);
+    expect(app.formLogin.valid).toBe(true);
   });
 });
