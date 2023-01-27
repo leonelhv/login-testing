cd4b7989ff0fe19773120695f5ee367bed68a44b
Leonel Huachaca <leonel.huachacav@gmail.com>
2023-01-27 09:19:22 -0500

    add login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'login-testing';
+
+  constructor(private fb: FormBuilder) {}
+
+  formLogin = this.fb.group({
+    usuario: ['', [Validators.required, Validators.minLength(5)]],
+    password: ['', [Validators.required, Validators.minLength(4)]],
+  });
+
+  get form() {
+    return this.formLogin.controls;
+  }
+
+  campoNoValido(campo: string) {
+    return (
+      this.formLogin.get(campo)?.invalid && this.formLogin.get(campo)?.touched
+    );
+  }
 }
